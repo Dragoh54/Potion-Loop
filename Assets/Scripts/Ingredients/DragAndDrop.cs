e0038a72856050b109e4623cb2dca97101032aa5
Dragoh54 <ndragoh@gmail.com>
2025-08-01 01:46:48 +0300

    feat: minor changes

--- Assets/Scripts/Ingredients/DragAndDrop.cs
+++ Assets/Scripts/Ingredients/DragAndDrop.cs
@@ -16,7 +16,7 @@ namespace Ingredients
         {
             if (_isDrag)
             {
-                Vector3 previousPosition = transform.position;
+                var previousPosition = transform.position;
 
                 _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 _newPos = _mousePos + _offset;
@@ -25,15 +25,7 @@ namespace Ingredients
 
                 //TODO: Remove if we don't need rotation
                 //dragged rotation idk if we need this just added
-                var direction = _newPos - previousPosition;
-
-                if (direction.sqrMagnitude > 0.00001f) 
-                {
-                    var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                    _targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
-                }
-                
-                transform.rotation = Quaternion.Lerp(transform.rotation, _targetRotation, Time.deltaTime * rotationLerpSpeed);
+                RotateTowardsMouse(previousPosition);
             }
         }
 
@@ -48,6 +40,19 @@ namespace Ingredients
         protected virtual void OnMouseUp()
         {
             _isDrag = false;
+        }
+
+        private void RotateTowardsMouse(Vector3 previousPosition)
+        {
+            var direction = _newPos - previousPosition;
+
+            if (direction.sqrMagnitude > 0.00001f) 
+            {
+                var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                _targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+                
+            transform.rotation = Quaternion.Lerp(transform.rotation, _targetRotation, Time.deltaTime * rotationLerpSpeed);
         }
     }
 }
