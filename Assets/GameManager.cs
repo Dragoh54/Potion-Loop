8993c5803937fcd3f12c6c482a5c9e56eab2bc89
Anton Log <Antuandelog@outlook.com>
2025-08-02 12:55:30 +0300

    feat: add requiered recipe list in game manager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,16 +1,31 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [field: SerializeField] public DialogCanvas DialogCanvas { get; private set; }
     [field: SerializeField] public TextList DialogList { get; private set; }
+    [field: SerializeField] public PotionManager IngredientsManager { get; private set; }
+    [field: SerializeField] public RecipeBook RecipeBook { get; private set; }
+    [field: SerializeField] public List<Recipe> PresentRecipes { get; private set; }
 
     private int _currentCustomer = -1;
 
     private void Start()
     {
         DialogList = GetComponent<FileReader>().GetDialogText();
+        var recipes = RecipeBook.PresentRecipes;
+        var potionNames = DialogList.allStory.Select(customer => customer.potion).ToList();
+
+        foreach (var potion in potionNames)
+        {
+            var correspondingRecipe = recipes.FirstOrDefault(recipe => recipe.Name == potion);
+
+            PresentRecipes.Add(correspondingRecipe);
+        }
+
         ChangeCustomer();
     }
 
