b5f6466971619e8cb807deeb5936d836fdb1d947
Dragoh54 <ndragoh@gmail.com>
2025-08-03 11:42:14 +0300

    feat: add water changing sprite

---
+++ Assets/Scripts/Ingredients/WaterIngredient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Ingredients
+{
+    public class WaterIngredient : MonoBehaviour
+    {
+        [SerializeField] private Sprite pickedUpWaterSprite;
+        [SerializeField] private Sprite stayedWaterSprite;
+        
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void OnMouseDown()
+        {
+            _spriteRenderer.sprite = pickedUpWaterSprite;
+        }
+
+        private void OnMouseUp()
+        {
+            _spriteRenderer.sprite = stayedWaterSprite;
+        }
+    }
+}
